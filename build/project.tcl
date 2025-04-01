97b2dda69475285bdd188c1c23068434a3d940f2
Kydinhvan <101254140+Kydinhvan@users.noreply.github.com>
2025-04-01 23:24:38 +0800

    added player and their bullet
    Co-authored-by: FredSterz <flgoat123@gmail.com>
    Co-authored-by: Natasha <natasha.sutd@gmail.com>

---
+++ build/project.tcl
@@ -0,0 +1,17 @@
+set projDir "./vivado"
+set projName "player_shoot"
+set topName top
+set device xc7a35tftg256-1
+if {[file exists "$projDir"]} { file delete -force "$projDir" }
+create_project $projName "$projDir" -part $device
+set_property design_mode RTL [get_filesets sources_1]
+set verilogSources [list "./source/abs.sv" "./source/adder.sv" "./source/alchitry_top.sv" "./source/alu.sv" "./source/alu_manual_tester.sv" "./source/bit_reverser.sv" "./source/boolean.sv" "./source/compact_shifter.sv" "./source/compare.sv" "./source/data_ram.sv" "./source/fa.sv" "./source/index_reverser.sv" "./source/multi_seven_seg.sv" "./source/multiplier.sv" "./source/mux_2.sv" "./source/mux_4.sv" "./source/player_mode.sv" "./source/rca.sv" "./source/seven_seg.sv" "./source/shifter.sv" "./source/ws2812b_driver.sv" "./source/x_bit_left_shifter.sv" "./source/reset_conditioner.sv" "./source/pipeline.sv" "./source/button_conditioner.sv" "./source/edge_detector.sv" "./source/simple_dual_port_ram.v" "./source/bin_to_dec.sv" "./source/counter.sv" "./source/decoder.sv" "./source/lucid_globals.sv" ]
+import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
+set xdcSources [list "./constraint/alchitry.xdc" "./constraint/au_props.xdc" ]
+read_xdc $xdcSources
+set_property STEPS.WRITE_BITSTREAM.ARGS.BIN_FILE true [get_runs impl_1]
+update_compile_order -fileset sources_1
+launch_runs -runs synth_1 -jobs 8
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
